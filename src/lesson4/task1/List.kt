dfd58c31de17bbc5684f484ccdbdd739ac320adb
httpsKitty <thepiratefox3000@gmail.com>
2021-10-09 20:35:36 +0300

    Update List.kt

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -241,7 +241,62 @@ fun decimalFromString(str: String, base: Int): Int = TODO()
  * 90 = XC, 100 = C, 400 = CD, 500 = D, 900 = CM, 1000 = M.
  * Например: 23 = XXIII, 44 = XLIV, 100 = C
  */
-fun roman(n: Int): String = TODO()
+fun roman(n: Int): String {
+    val units: String = when (n % 10) {
+        1 -> "I"
+        2 -> "II"
+        3 -> "III"
+        4 -> "IV"
+        5 -> "V"
+        6 -> "VI"
+        7 -> "VII"
+        8 -> "VIII"
+        9 -> "IX"
+        else -> ""
+    }
+
+    val tens: String = when (n / 10 % 10) {
+        1 -> "X"
+        2 -> "XX"
+        3 -> "XXX"
+        4 -> "XL"
+        5 -> "L"
+        6 -> "LX"
+        7 -> "LXX"
+        8 -> "LXXX"
+        9 -> "XC"
+        else -> ""
+    }
+
+    val hundreds: String = when (n / 100 % 10) {
+        1 -> "C"
+        2 -> "CC"
+        3 -> "CCC"
+        4 -> "CD"
+        5 -> "D"
+        6 -> "DC"
+        7 -> "DCC"
+        8 -> "DCCC"
+        9 -> "CM"
+        else -> ""
+    }
+
+    val thousands: String = when (n / 1000 % 10) {
+        1 -> "M"
+        2 -> "MM"
+        3 -> "MMM"
+        4 -> "MMMM"
+        5 -> "MMMMM"
+        6 -> "MMMMMM"
+        7 -> "MMMMMMM"
+        8 -> "MMMMMMMM"
+        9 -> "MMMMMMMMM"
+        else -> ""
+
+    }
+    return thousands + hundreds + tens + units
+
+}
 
 /**
  * Очень сложная (7 баллов)
