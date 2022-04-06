e4fb49c8f7762bf07d8fb0807bb61ea624b5d427
httpsKitty <90656318+httpsKitty@users.noreply.github.com>
2022-04-06 10:34:42 +0300

    Polynom

--- src/lesson11/task1/Polynom.kt
+++ src/lesson11/task1/Polynom.kt
@@ -1,6 +1,8 @@
 @file:Suppress("UNUSED_PARAMETER")
 
-package lesson11.task1
+
+import java.lang.ArithmeticException
+import kotlin.math.pow
 
 /**
  * Класс "полином с вещественными коэффициентами".
@@ -21,15 +23,28 @@ package lesson11.task1
  */
 class Polynom(vararg coeffs: Double) {
 
+    val coeffList = mutableListOf<Double>()
+
+
+    init {
+        for (i in coeffs.dropWhile { it == 0.0 }.reversed()) coeffList += i
+    }
+
+
     /**
      * Геттер: вернуть значение коэффициента при x^i
      */
-    fun coeff(i: Int): Double = TODO()
+    fun coeff(i: Int): Double = if (i < coeffList.size) coeffList[i]
+    else 0.0
+
 
     /**
      * Расчёт значения при заданном x
      */
-    fun getValue(x: Double): Double = TODO()
+    fun getValue(x: Double): Double = coeffList.foldIndexed(0.0) { index, sum, coeff ->
+        sum + coeff * x.pow(index)
+    }
+
 
     /**
      * Степень (максимальная степень x при ненулевом слагаемом, например 2 для x^2+x+1).
@@ -38,27 +53,66 @@ class Polynom(vararg coeffs: Double) {
      * Слагаемые с нулевыми коэффициентами игнорировать, т.е.
      * степень 0x^2+0x+2 также равна 0.
      */
-    fun degree(): Int = TODO()
+    fun degree(): Int = maxOf(0, coeffList.lastIndex)
 
     /**
      * Сложение
      */
-    operator fun plus(other: Polynom): Polynom = TODO()
+    operator fun plus(other: Polynom): Polynom {
+        val resultCoeffs = mutableListOf<Double>()
+        for (i in 0..maxOf(this.degree(), other.degree())) {
+            resultCoeffs.add(this.coeff(i) + other.coeff(i))
+        }
+        return Polynom(*resultCoeffs.toDoubleArray().reversedArray())
+    }
 
     /**
      * Смена знака (при всех слагаемых)
      */
-    operator fun unaryMinus(): Polynom = TODO()
+    operator fun unaryMinus(): Polynom = Polynom(*coeffList.map { -it }.toDoubleArray().reversedArray())
 
     /**
      * Вычитание
      */
-    operator fun minus(other: Polynom): Polynom = TODO()
+    operator fun minus(other: Polynom): Polynom = this + (-other)
 
     /**
      * Умножение
      */
-    operator fun times(other: Polynom): Polynom = TODO()
+    operator fun times(other: Polynom): Polynom {
+        val resultCoeffs = MutableList(this.degree() + other.degree() + 1) { 0.0 }
+        this.coeffList.forEachIndexed { i, thisCoeff ->
+            other.coeffList.forEachIndexed { j, otherCoeff ->
+                resultCoeffs[i + j] += thisCoeff * otherCoeff
+            }
+        }
+        return Polynom(*resultCoeffs.toDoubleArray().reversedArray())
+    }
+
+
+    private fun divide(other: Polynom): Pair<Polynom, Polynom> {
+        if (other.degree() == 0) return divide(other.coeff(0))
+        var quotinent = Polynom()
+        var remainder = Polynom(*coeffList.reversed().toDoubleArray())
+        while (remainder.degree() >= other.degree()) {
+            val multiplierDegree = remainder.degree() - other.degree()
+            val multiplierCoeff = remainder.coeff(remainder.degree()) / other.coeff(other.degree())
+            val multiplier = Polynom(*DoubleArray(multiplierDegree + 1) { i ->
+                if (i == multiplierDegree) multiplierCoeff
+                else 0.0
+            }.reversedArray())
+            quotinent += multiplier
+            remainder -= multiplier * other
+        }
+        return quotinent to remainder
+    }
+
+    private fun divide(n: Double): Pair<Polynom, Polynom> {
+        if (n == 0.0) throw ArithmeticException("Cannot divide by zero")
+        return Polynom(
+            *coeffList.map { it / n }.reversed().toDoubleArray()
+        ) to Polynom()
+    }
 
     /**
      * Деление
@@ -68,20 +122,26 @@ class Polynom(vararg coeffs: Double) {
      *
      * Если A / B = C и A % B = D, то A = B * C + D и степень D меньше степени B
      */
-    operator fun div(other: Polynom): Polynom = TODO()
+    operator fun div(other: Polynom): Polynom = divide(other).first
 
     /**
      * Взятие остатка
      */
-    operator fun rem(other: Polynom): Polynom = TODO()
+    operator fun rem(other: Polynom): Polynom = divide(other).second
+
 
     /**
      * Сравнение на равенство
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Polynom) return false
+        return coeffList == other.coeffList
+    }
+
 
     /**
      * Получение хеш-кода
      */
-    override fun hashCode(): Int = TODO()
+    override fun hashCode() = coeffList.hashCode()
 }
